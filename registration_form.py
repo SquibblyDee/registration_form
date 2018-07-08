1f85eb93a8fbe125d7a91e0a4682d50690c71fd4
ste ste <steelst0@gmail.com>
2018-07-07 21:19:11 -0700

    functional meets all reqs

--- registration_form.py
+++ registration_form.py
@@ -8,21 +8,34 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     # Block that checks if all form fields are populated
+    error = False;
     if len(request.form['email'])< 1:
         flash('Email field required!', 'error')
+        error = True;
     elif len(request.form['first_name'])< 1:
         flash('First name field required!', 'error')
+        error = True;
     elif len(request.form['last_name'])< 1:
         flash('Last name field required!', 'error')
+        error = True;
     elif len(request.form['password']) < 1:
         flash('Password field required!', 'error')
+        error = True;
     elif len(request.form['confirm_password'])< 1:
         flash('Confirm field required!', 'error')
+        error = True;
     elif len(request.form['password']) < 9:
         flash('Password minimum 9 characters!')
+        error = True;
     elif request.form['password'] != request.form['confirm_password']:
         flash('Passwords must match!')
-    return redirect('/')
+        error = True;
+
+    if error == True:
+        return redirect('/')
+    else:
+        flash('Thanks for submitting your information!')
+        return redirect('/')
 
     
 
