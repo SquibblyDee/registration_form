0457a5ad77461aa6dd4bc82536c331eb0d3901de
ste ste <steelst0@gmail.com>
2018-07-07 20:19:59 -0700

    initial commit

---
+++ registration_form.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, request, flash
+app = Flask(__name__)
+app.secret_key = "boobookittycluck"
+@app.route('/')
+def index():
+    return render_template('registration_form.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    # Block that checks if all form fields are populated
+    if len(request.form['email'])< 1:
+        flash('Email field required!', 'error')
+    elif len(request.form['first_name'])< 1:
+        flash('First name field required!', 'error')
+    elif len(request.form['last_name'])< 1:
+        flash('Last name field required!', 'error')
+    elif len(request.form['password'])< 1:
+        flash('Password field required!', 'error')
+    elif len(request.form['confirm_password'])< 1:
+        flash('Confirm field required!', 'error')
+    else:
+        flash('Thanks for submitting your information.')
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
