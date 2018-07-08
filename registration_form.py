05f019cd620d5ece63500c33b5e260e0b38ef723
ste ste <steelst0@gmail.com>
2018-07-07 20:50:57 -0700

    test revert

--- registration_form.py
+++ registration_form.py
@@ -14,10 +14,14 @@ def process():
         flash('First name field required!', 'error')
     elif len(request.form['last_name'])< 1:
         flash('Last name field required!', 'error')
-    elif len(request.form['password'])< 1:
+    elif len(request.form['password']) < 1:
         flash('Password field required!', 'error')
+    elif len(request.form['password']) < 9:
+        flash('Password minimum 9 characters!')
     elif len(request.form['confirm_password'])< 1:
         flash('Confirm field required!', 'error')
+    elif request.form['password'] != request.form['confirm_password']:
+        flash('Must match password field!')
     else:
         flash('Thanks for submitting your information.')
     return redirect('/')
